ecffa7e796d1de82dc633c7ead583138b5df06ab
Diana Bowler <diana.bowler@idiv.de>
2021-12-23 21:27:46 +0100

    check application of function  - all good

--- 04_core_margin.R
+++ 04_core_margin.R
@@ -7,9 +7,6 @@ library(raster)
 
 ### subset data #####
 
-#just to test below code
-modelSummaries$PA <- sapply(modelSummaries$mean, function(x) rbinom(1,1,x))
-
 #add on lon and lat
 modelSummaries$lon <- mtbsDF$lon[match(modelSummaries$MTB, mtbsDF$MTB)]
 modelSummaries$lat <- mtbsDF$lat[match(modelSummaries$MTB, mtbsDF$MTB)]
@@ -26,59 +23,9 @@ allyears <- sort(unique(modelSummaries_Limits$Year))
 
 utmProj <- "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 
-### example species #####
+### apply function #####
 
-
-getCoreRegions <- function(myspecies,myyear){
-
-speciesSummary <- modelSummaries_Limits %>%
-                  filter(Species == myspecies) %>%
-                  filter(Year == myyear)
-
-speciesPixels <- SpatialPixelsDataFrame(points = as.matrix(speciesSummary[,c("lon","lat")]),
-                                        data = data.frame(PA=speciesSummary$PA),
-                                        tolerance = 0.6,
-                                        proj4string = crs("+proj=longlat +datum=WGS84"))
-
-#make into a raster
-speciesRaster <- raster(speciesPixels)
-
-#for each raster cell, get mean class of surroundings
-## in a 3-by-3 window surrounding each focal cell 
-rmean <- focal(speciesRaster, 
-               w = matrix(1, ncol=3, nrow=3), 
-               fun=mean, na.rm=TRUE)
-#plot(rmean)
-
-#define core as all those fully surrounded by presences
-speciesRasterCore <- rmean
-speciesRasterCore[speciesRasterCore < 1] <- 0.5 #indicator for marginal sites
-speciesRasterCore[is.na(speciesRaster)] <- NA
-speciesRasterCore[speciesRaster==0] <- 0 #absent sites
-plot(speciesRasterCore)
-
-#convert back into a data frame
-coreDF <- as.data.frame(speciesRasterCore, xy=TRUE)
-coreDF$Obs <- as.data.frame(speciesRaster)[,1] #original observation
-coreDF$cellNu <- cellFromXY(speciesRaster, coreDF[,c("x","y")])
-coreDF <- subset(coreDF, !is.na(layer)) #remove sites beyond german border
-
-#clean marginal information
-coreDF$Core <- ifelse(coreDF$layer==1,"core",
-                      ifelse(coreDF$layer==0.5, "marginal", "absent"))
-
-#add on another data
-coreDF$Year <- myyear
-coreDF$Species <- myspecies
-
-return(coreDF)
-
-}
-
-### apply function ###
-
-getCoreRegions(allspecies[1],allyears[1])
-
+getCoreRegions(allspecies[1])
 
 #other possibilities
 

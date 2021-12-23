f6a9bf29fc671bfe063d40ba8114189bfcf8be21
Diana Bowler <diana.bowler@idiv.de>
2021-12-23 18:13:46 +0100

    function to identify cores using a moving window

--- 04_core_margin.R
+++ 04_core_margin.R
@@ -1,6 +1,4 @@
 #script to pull out core and marginal areas
-
-library(landscapemetrics)
 library(raster)
 
 ### get data ####
@@ -24,25 +22,65 @@ modelSummaries_Limits <- subset(modelSummaries, Year %in% c(1990,2016))
 
 allspecies <- sort(unique(modelSummaries_Limits$Species))
 
-allYears <- sort(unique(modelSummaries_Limits$Year))
+allyears <- sort(unique(modelSummaries_Limits$Year))
 
 utmProj <- "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 
 ### example species #####
 
-speciesSummary <- subset(modelSummaries_Limits,
-                         Species = allspecies[1] & Year == 2010)
-speciesRaster <- SpatialPixelsDataFrame(points = as.matrix(speciesSummary[,c("lon","lat")]),
+
+getCoreRegions <- function(myspecies,myyear){
+
+speciesSummary <- modelSummaries_Limits %>%
+                  filter(Species == myspecies) %>%
+                  filter(Year == myyear)
+
+speciesPixels <- SpatialPixelsDataFrame(points = as.matrix(speciesSummary[,c("lon","lat")]),
                                         data = data.frame(PA=speciesSummary$PA),
                                         tolerance = 0.6,
                                         proj4string = crs("+proj=longlat +datum=WGS84"))
 
 #make into a raster
-speciesRaster <- raster(speciesRaster)
+speciesRaster <- raster(speciesPixels)
 
-#convert to utm projection
-speciesRaster <- projectRaster(speciesRaster, crs=utmProj, method="ngb")
-plot(speciesRaster)
+#for each raster cell, get mean class of surroundings
+## in a 3-by-3 window surrounding each focal cell 
+rmean <- focal(speciesRaster, 
+               w = matrix(1, ncol=3, nrow=3), 
+               fun=mean, na.rm=TRUE)
+#plot(rmean)
+
+#define core as all those fully surrounded by presences
+speciesRasterCore <- rmean
+speciesRasterCore[speciesRasterCore < 1] <- 0.5 #indicator for marginal sites
+speciesRasterCore[is.na(speciesRaster)] <- NA
+speciesRasterCore[speciesRaster==0] <- 0 #absent sites
+plot(speciesRasterCore)
+
+#convert back into a data frame
+coreDF <- as.data.frame(speciesRasterCore, xy=TRUE)
+coreDF$Obs <- as.data.frame(speciesRaster)[,1] #original observation
+coreDF$cellNu <- cellFromXY(speciesRaster, coreDF[,c("x","y")])
+coreDF <- subset(coreDF, !is.na(layer)) #remove sites beyond german border
+
+#clean marginal information
+coreDF$Core <- ifelse(coreDF$layer==1,"core",
+                      ifelse(coreDF$layer==0.5, "marginal", "absent"))
+
+#add on another data
+coreDF$Year <- myyear
+coreDF$Species <- myspecies
+
+return(coreDF)
+
+}
+
+### apply function ###
+
+getCoreRegions(allspecies[1],allyears[1])
+
+
+#other possibilities
 
 #define core and marginal areas
 #core area is defined as all cells that are further away from the edge of each patch than a specified edge depth
@@ -52,22 +90,19 @@ plot(speciesRaster)
 show_patches(speciesRaster)
 show_cores(speciesRaster) #inside areas beyond an edge
 
-show_lsm(speciesRaster,what = "lsm_p_area")
-show_lsm(speciesRaster,what = "lsm_p_core")
-show_lsm(speciesRaster,what = "lsm_p_cai", edge_depth=1)
-show_lsm(speciesRaster,what = "lsm_p_frac")
+#explore possibilities
+show_lsm(speciesRaster, class="1", what = "lsm_p_area")
+show_lsm(speciesRaster, class="1",what = "lsm_p_core")
+show_lsm(speciesRaster, class="1",what = "lsm_p_cai", edge_depth=10)
+show_lsm(speciesRaster, class="1",what = "lsm_p_frac")
+show_lsm(speciesRaster, class="1",what = "lsm_p_contig")
+show_lsm(speciesRaster, class="1",what = "lsm_p_contig")
+show_lsm(speciesRaster, class="1",what = "lsm_p_para")
+show_lsm(speciesRaster, class="1",what = "lsm_p_ncore")
 
-
-lsm_p_core(speciesRaster)
-temp <- lsm_p_cai(speciesRaster)
-
+#Get data frame
 rasterCore <- spatialize_lsm(speciesRaster, what = "lsm_p_core")
 rasterCai <- spatialize_lsm(speciesRaster, what = "lsm_p_cai")
 
 plot(rasterCai$layer_1$lsm_p_cai)
 plot(rasterCore$layer_1$lsm_p_core)
-
-myRaster <- spatialize_lsm(speciesRaster, what = "lsm_p_area")
-plot(myRaster$layer_1$lsm_p_area)
-
-speciesRaster[speciesRaster==0] <- NA

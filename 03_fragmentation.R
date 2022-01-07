f8b5a268fb5affa89db9aee5c9aa3ddaaa03a9c8
Diana Bowler <diana.bowler@idiv.de>
2022-01-07 19:57:19 +0100

    test relationship between clumpiness and AOCC

--- 03_fragmentation.R
+++ 03_fragmentation.R
@@ -37,70 +37,6 @@ allYears <- sort(unique(modelSummaries_Limits$Year))
 
 utmProj <- "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
 
-### example species #####
-
-# speciesSummary <- subset(modelSummaries_Limits, 
-#                          Species = allspecies[1] & Year == 2010)
-# speciesRaster <- SpatialPixelsDataFrame(points = as.matrix(speciesSummary[,c("lon","lat")]),
-#                                         data = data.frame(PA=speciesSummary$PA),
-#                                         tolerance = 0.6,
-#                                         proj4string = crs("+proj=longlat +datum=WGS84"))
-# 
-# #make into a raster
-# speciesRaster <- raster(speciesRaster)
-# 
-# #convert to utm projection
-# speciesRaster <- projectRaster(speciesRaster, crs=utmProj, method="ngb")
-# plot(speciesRaster)
-
-### function ####
-
-#make raster stack for a species - layers represent each year
-#just take first and last year
-getFragStats <- function(species, modelSummaries_Limits){
-  
-    #data for 1990
-    speciesSummary <- modelSummaries_Limits %>%
-      filter(Species==species) %>%
-      filter(Year == 1990)
-    
-    speciesRaster <- SpatialPixelsDataFrame(points = as.matrix(speciesSummary[,c("lon","lat")]),
-                                            data = data.frame(PA=speciesSummary$PA),
-                                            tolerance = 0.6,
-                                            proj4string = crs("+proj=longlat +datum=WGS84"))
-    
-    #make into a raster
-    r1 <- raster(speciesRaster)
-    
-    #data for 2016
-    speciesSummary <- modelSummaries_Limits %>%
-      filter(Species==species) %>%
-      filter(Year == 2016)
-    
-    speciesRaster <- SpatialPixelsDataFrame(points = as.matrix(speciesSummary[,c("lon","lat")]),
-                                            data = data.frame(PA=speciesSummary$PA),
-                                            tolerance = 0.6,
-                                            proj4string = crs("+proj=longlat +datum=WGS84"))
-    
-    #make into a raster
-    r2 <- raster(speciesRaster)
-    
-
-  #make stack  
-  speciesStack <- stack(list(r1,r2))
-  speciesStack <- projectRaster(speciesStack, crs=utmProj, method="ngb")
-  
-  #calc metrics
-  calculate_lsm(speciesStack, 
-                what = c("lsm_l_ta",
-                         "lsm_c_pland",
-                         "lsm_c_clumpy"),
-                full_name = TRUE) %>%
-    add_column(Year = c(rep(1990,5),rep(2016,5))) %>%
-    add_column(Species = species)
-}
-
-
 ### apply ####
 
 #get realizations
@@ -108,10 +44,54 @@ PAs <- lapply(modelSummaries_Limits$mean, function(x) rbinom(5,1,x))
 PA_matrix <- do.call(rbind,PAs)
 
 #apply it
-fragDF <- lapply(allspecies,function(x){
+fragDF <- lapply(allspecies[-1],function(x){
                  applyFragStats(x, modelSummaries_Limits, summary = "annual")
 })
 fragDF <- do.call(rbind,fragDF)
+
+### plotting ####
+
+fragDF <- fragDF %>%
+  filter(class == 1) %>%
+  filter(name == "clumpiness index") %>%
+  group_by(Species) %>%
+  mutate(change = log(medianMetric[Year==2016]/medianMetric[Year==1990]))
+save(fragDF, file="outputs/clumpiChanges.rds")
+
+fragDF%>%
+  ggplot(aes(x=Year,y=medianMetric,group=Species,colour=change))+
+  scale_colour_gradient2(low="green",high="purple")+ 
+  geom_point(alpha=0.2)+
+  geom_line(size=2,alpha=0.5)+
+  scale_x_continuous(breaks=c(1990,2016),labels=c(1990,2016))+
+  ylab("Clumpiness")
+
+ggsave("plots/clumpiChange.png")
+
+### rel with AOCC
+
+fragDF <- lapply(allspecies[-1],function(x){
+  applyFragStats(x, modelSummaries_Limits, summary = "change")
+})
+fragChanges <- do.call(rbind,fragDF)
+
+areaChanges <- readRDS("outputs/areaChanges.rds")
+
+allChanges <- inner_join(fragChanges,areaChanges,
+                         by=c("species"),
+                         suffix = c("_frag","_area"))
+
+ggplot(data = allChanges,
+       aes(x = medianChange_area,y = medianChange_frag)) + 
+  geom_point() + 
+  geom_errorbar(aes(ymin = lowerChange_frag,ymax = upperChange_frag)) + 
+  geom_errorbarh(aes(xmin = lowerChange_area, xmax = upperChange_area))+
+  geom_vline(linetype="dashed",xintercept=0)+
+  xlab("Change in AOCC") + ylab("Change in clumpiness")+
+  theme_few()
+
+ggsave("plots/eoccChange_vs_clumpiChange.png")
+
 
 ### Appendix: metric play ####
 
@@ -134,43 +114,43 @@ fragDF <- do.call(rbind,fragDF)
 # ## lsm_p_"metric"
 # lsm_l_enn()
 
-#what is available
-
-list_lsm()
-
-list_lsm()$name
-
-list_lsm(level = 'patch')
-
-list_lsm(level = "class")$type
-
-list_lsm(name = "total core area")
-
-list_lsm(level = "landscape")$type
-
-#visualizations
-
-show_patches(speciesRaster, class="all")
-
-show_cores(speciesRaster, class="all") #inside areas beyond an edge
-
-show_lsm(speciesRaster, what = "lsm_p_contig")
-
-#spatialize_lsm(speciesRaster, what = "lsm_p_contig")
-
-#correlations
-
-metrics <- calculate_lsm(speciesRaster, what = c("class"))
-correlations <- calculate_correlation(metrics)
-show_correlation(data = correlations, method = "pearson")
-
-#apply a function
-lsm_c_clumpy(speciesRaster)
-lsm_c_clumpy(speciesStack)
-
-#appply multiple functions
-calculate_lsm(speciesRaster, 
-              what = c("lsm_l_ta",
-                       "lsm_c_pland",
-                       "lsm_c_clumpy"),
-              full_name = TRUE)
+# #what is available
+# 
+# list_lsm()
+# 
+# list_lsm()$name
+# 
+# list_lsm(level = 'patch')
+# 
+# list_lsm(level = "class")$type
+# 
+# list_lsm(name = "total core area")
+# 
+# list_lsm(level = "landscape")$type
+# 
+# #visualizations
+# 
+# show_patches(speciesRaster, class="all")
+# 
+# show_cores(speciesRaster, class="all") #inside areas beyond an edge
+# 
+# show_lsm(speciesRaster, what = "lsm_p_contig")
+# 
+# #spatialize_lsm(speciesRaster, what = "lsm_p_contig")
+# 
+# #correlations
+# 
+# metrics <- calculate_lsm(speciesRaster, what = c("class"))
+# correlations <- calculate_correlation(metrics)
+# show_correlation(data = correlations, method = "pearson")
+# 
+# #apply a function
+# lsm_c_clumpy(speciesRaster)
+# lsm_c_clumpy(speciesStack)
+# 
+# #appply multiple functions
+# calculate_lsm(speciesRaster, 
+#               what = c("lsm_l_ta",
+#                        "lsm_c_pland",
+#                        "lsm_c_clumpy"),
+#               full_name = TRUE)

49dc210d8bdd2eaf9c9066b2747d81ee45e2aa38
Diana Bowler <diana.bowler@idiv.de>
2022-01-07 20:43:27 +0100

    start summary of changes in core and marginal areas

--- 04_core_margin.R
+++ 04_core_margin.R
@@ -5,6 +5,8 @@ library(raster)
 
 #run script 01
 
+source("05_core_functions.R")
+
 ### subset data #####
 
 #add on lon and lat
@@ -25,8 +27,25 @@ utmProj <- "+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_
 
 ### apply function #####
 
-getCoreRegions(allspecies[1])
+temp <- getCoreRegions(allspecies[5])
 
+#get realizations
+PAs <- lapply(modelSummaries_Limits$mean, function(x) rbinom(100,1,x))
+PA_matrix <- do.call(rbind,PAs)
+
+#add on data
+modelSummaries_Limits$PA <- PA_matrix[,1] 
+modelSummaries_Limits$Core <- temp$Core[match(modelSummaries_Limits$MTB, temp$MTB)]
+
+#summarize
+coreSummary <- modelSummaries_Limits %>%
+                  filter(!is.na(Core)) %>%
+                  group_by(Core, Year) %>%
+                  summarize(occ = sum(PA), total= length(PA)) %>%
+                  mutate(prop = occ/total)
+
+
+### appendix ####
 #other possibilities
 
 #define core and marginal areas
@@ -34,22 +53,22 @@ getCoreRegions(allspecies[1])
 
 #https://github.com/r-spatialecology/landscapemetrics/blob/main/R/show_cores.R
 
-show_patches(speciesRaster)
-show_cores(speciesRaster) #inside areas beyond an edge
-
-#explore possibilities
-show_lsm(speciesRaster, class="1", what = "lsm_p_area")
-show_lsm(speciesRaster, class="1",what = "lsm_p_core")
-show_lsm(speciesRaster, class="1",what = "lsm_p_cai", edge_depth=10)
-show_lsm(speciesRaster, class="1",what = "lsm_p_frac")
-show_lsm(speciesRaster, class="1",what = "lsm_p_contig")
-show_lsm(speciesRaster, class="1",what = "lsm_p_contig")
-show_lsm(speciesRaster, class="1",what = "lsm_p_para")
-show_lsm(speciesRaster, class="1",what = "lsm_p_ncore")
-
-#Get data frame
-rasterCore <- spatialize_lsm(speciesRaster, what = "lsm_p_core")
-rasterCai <- spatialize_lsm(speciesRaster, what = "lsm_p_cai")
-
-plot(rasterCai$layer_1$lsm_p_cai)
-plot(rasterCore$layer_1$lsm_p_core)
+# show_patches(speciesRaster)
+# show_cores(speciesRaster) #inside areas beyond an edge
+# 
+# #explore possibilities
+# show_lsm(speciesRaster, class="1", what = "lsm_p_area")
+# show_lsm(speciesRaster, class="1",what = "lsm_p_core")
+# show_lsm(speciesRaster, class="1",what = "lsm_p_cai", edge_depth=10)
+# show_lsm(speciesRaster, class="1",what = "lsm_p_frac")
+# show_lsm(speciesRaster, class="1",what = "lsm_p_contig")
+# show_lsm(speciesRaster, class="1",what = "lsm_p_contig")
+# show_lsm(speciesRaster, class="1",what = "lsm_p_para")
+# show_lsm(speciesRaster, class="1",what = "lsm_p_ncore")
+# 
+# #Get data frame
+# rasterCore <- spatialize_lsm(speciesRaster, what = "lsm_p_core")
+# rasterCai <- spatialize_lsm(speciesRaster, what = "lsm_p_cai")
+# 
+# plot(rasterCai$layer_1$lsm_p_cai)
+# plot(rasterCore$layer_1$lsm_p_core)

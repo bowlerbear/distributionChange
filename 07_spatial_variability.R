56f8b0cc1858c12967a6fc39d4bc5f1545fae3b7
Diana Bowler <diana.bowler@idiv.de>
2022-02-01 21:11:09 +0100

    playing around with standard deviation of change

--- 07_spatial_variability.R
+++ 07_spatial_variability.R
@@ -7,29 +7,58 @@ source("05_core_functions.R")
 PAs <- lapply(modelSummaries_Limits$mean, function(x) rbinom(50,1,x))
 PA_matrix <- do.call(rbind,PAs)
 
-### saturation #####
+### turnover ####
 
-#need to sort out units
+#Jaccard turnover index
 
-saturationDF <- lapply(allspecies[-c(1,64)],function(x){
-  applySaturation(x, modelSummaries_Limits, summary = "change")
-}) %>% reduce(rbind)
+#turnover - probability of a random selected patch occupied at time 1, 
+#is still occupied at time 2
+#of patches occupied at time 1, how many are occupied at time 2
 
-saveRDS(saturationDF, file="outputs/satuationChange.rds")
+#null is that total change is driven all by increase or decrease 
 
-### plotting ####
+### get SDs ####
 
-allChanges <- saturationDF  %>%
-  inner_join(.,areaChanges,
-             by=c("species"),
-             suffix = c("_sat","_area"))
+sdChanges <- lapply(allspecies,function(x){
+  applyChangeSD(x, modelSummaries_Limits)
+}) %>% 
+  reduce(rbind)%>%
+  rename(species="Species")
+saveRDS(sdChanges, file="outputs/sdChanges.rds")
 
-g2 <- ggplot(data = allChanges,
-             aes(x = medianChange_area,y = medianChange_sat)) + 
-  geom_point() + 
-  geom_hline(yintercept=0,linetype="dashed")+
-  stat_smooth(method="gam")+
-  geom_vline(linetype="dashed",xintercept=0)+
-  xlab("Change in AOC") + ylab("Change in saturation")
+#boxplots
 
-g2
+sdChanges$Direction <- areaChanges$Direction[match(sdChanges$species,
+                                                           areaChanges$species)]
+
+(g1 <- ggplot(sdChanges, aes(x = Direction, y = medianChange)) +
+    geom_pirate(aes(colour = Direction, fill = Direction),bars=FALSE) +
+    xlab("Direction of change") + ylab("Turnover") +
+    scale_colour_brewer(type="qual", direction=-1) +
+    scale_fill_brewer(type="qual", direction=-1))
+
+### relationships ####
+
+allChanges <- inner_join(sdChanges,areaChanges,
+                         by=c("species"),
+                         suffix = c("_sd","_area"))
+
+(g1 <- ggplot(data = allChanges,
+              aes(x = medianChange_area,y = medianChange_sd)) + 
+    geom_point() + 
+    #geom_smooth(method="gam") +
+    geom_errorbar(aes(ymin = lowerChange_sd,ymax = upperChange_sd)) + 
+    geom_errorbarh(aes(xmin = lowerChange_area, xmax = upperChange_area))+
+    scale_colour_viridis_c()+
+    geom_hline(linetype="dashed",yintercept=0)+
+    geom_vline(linetype="dashed",xintercept=0)+
+    xlab("Change in AOO") + ylab("Turnover"))
+
+
+### boxplots ####
+
+p1 <- readRDS("plots/boxplot_saturationChange.rds")
+p2 <- readRDS("plots/boxplot_areaChange.rds")
+p3 <- readRDS("plots/boxplot_hullChange.rds")
+p4 <- readRDS("plots/boxplot_fragChange_change.rds")
+plot_grid(p2,p3,p1,p4,nrow=2)
